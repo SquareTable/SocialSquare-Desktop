8384b0d1b6c589780715b1469120fbf05ad611fb
sebtheman <84299475+Sebastian-Webster@users.noreply.github.com>
2022-01-25 22:24:57 +1300

    add windows and macos support

---
+++ windows/SocialSquareDesktop/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::SocialSquareDesktop::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::SocialSquareDesktop::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
