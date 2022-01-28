0afbb4cc7f0415b3f4704e0c03350a6a48cdec19
sebtheman <84299475+Sebastian-Webster@users.noreply.github.com>
2022-01-29 05:55:08 +1300

    strip everything unneccesary to fix things

--- ios/Pods/Target Support Files/Pods-SocialSquareDesktop-SocialSquareDesktopTests/Pods-SocialSquareDesktop-SocialSquareDesktopTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SocialSquareDesktop_SocialSquareDesktopTests : NSObject
-@end
-@implementation PodsDummy_Pods_SocialSquareDesktop_SocialSquareDesktopTests
-@end
--- ios/Pods/Target Support Files/Pods-SocialSquareDesktop/Pods-SocialSquareDesktop-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SocialSquareDesktop : NSObject
-@end
-@implementation PodsDummy_Pods_SocialSquareDesktop
-@end
